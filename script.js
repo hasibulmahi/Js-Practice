109b29455b5d5ef90b8eaff7155c05935f7cd05d
hasibulmahi <hasibulmahi@gmail.com>
2023-05-02 00:31:52 +0600

    first-commit

---
+++ script.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cupiditate, culpa eos cum architecto labore facere doloribus vero illo maxime sunt iure commodi perferendis voluptatem velit, explicabo fuga nemo neque. ";
+}
