dcde001a66ea53827552ac24e17795a6a2cef104
hasibulmahi <hasibulmahi@gmail.com>
2023-05-02 23:50:55 +0600

    first-commit

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cupiditate, culpa eos cum architecto labore facere doloribus vero illo maxime sunt iure commodi perferendis voluptatem velit, explicabo fuga nemo neque. ";
+    document.getElementById("demo1").innerHTML = "Hello World!";
+    document.getElementById("demo2").innerHTML = "How are you?";
 }
+// let a, b, c;
+// a = 5;
+// b = 6;
+// c = a + b;
+// document.getElementById("demo").innerHTML = c;
+// let x, y, z; //statement 1
+// x = 5; //statement 2 
+// y = 6; //statement 3
+// z = x + y; //statement 4
+
+// document.getElementById("demo").innerHTML = "The value of z is " + z + ".";
+
+// console.log(5 + 5);
+// alert(50 + 50);
+// window.alert(50 + 50);
+// document.write(5 + 5);
+// document.getElementById("demo").innerHTML = 5 + 6;
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cupiditate, culpa eos cum architecto labore facere doloribus vero illo maxime sunt iure commodi perferendis voluptatem velit, explicabo fuga nemo neque. "; 
+// }
