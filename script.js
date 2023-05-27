802e74ee441f35fc2964fe74e1d848ae41826db2
hasibulmahi <hasibulmahi@gmail.com>
2023-05-27 23:57:02 +0600

    first-commit

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-// Assign the value 5 to x
-let x = 10;
-// Assign the value 2 to y
-let y = 5;
-// Assign the value x + y to z
-let z = x * y;
-//Display z
-document.getElementById("demo").innerHTML = "The Sum of x + y is: " + z;
+let a = 3;
+let x = (100 + 50) * a;
+document.getElementById("demo").innerHTML = x;
+// // Assign the value 5 to x
+// let x = 10;
+// // Assign the value 2 to y
+// let y = 5;
+// // Assign the value x + y to z
+// let z = x * y;
+// //Display z
+// document.getElementById("demo").innerHTML = "The Sum of x + y is: " + z;
 // let x = 10;
 // document.getElementById("demo").innerHTML = x;
 // // create an object:
