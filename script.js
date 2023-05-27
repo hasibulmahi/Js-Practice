8615454d014877f2a6e8e21d067a09f61a5a1856
hasibulmahi <hasibulmahi@gmail.com>
2023-05-28 01:05:29 +0600

    first-commit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let a = 3;
-let x = (100 + 50) * a;
+var x = 10;
+x += 5; // x = x+5;
 document.getElementById("demo").innerHTML = x;
+
+// let a = 3;
+// let x = (100 + 50) * a;
+// document.getElementById("demo").innerHTML = x;
 // // Assign the value 5 to x
 // let x = 10;
 // // Assign the value 2 to y
